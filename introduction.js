3ba908e9f95a4d483cd114869fbce9b05f2c7636
Sharon-Njung'e <njungesharonwanjiku@gmail.com>
2024-04-15 21:48:13 +0300

    add files

---
+++ introduction.js
@@ -0,0 +1,1 @@
+console.log("Welcome to my 30 Days of javascript");
---
+++ variables.js
@@ -0,0 +1,15 @@
+// Creating variables with var, let, const keywords.
+var a = "Jeep"
+console.log(a);
+
+let b = "Karen"
+console.log(b);
+
+const c = "Akirachix"
+console.log(c);
+
+// We use const if the value should not be changed
+// var when the value is mutable
+
+const me = "MaryAnn"
+console.log(me);
